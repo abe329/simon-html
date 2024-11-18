bb748d10c5042394add46dadd787b475d376a8fe
abe329 <abehull23@gmail.com>
2024-11-18 14:24:32 -0700

    Committing React

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './about.css';
 
 export function About() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>about displayed here</div>
+      <div id="picture" class="picture-box"><img width="400px" src="placeholder.jpg" alt="random" /></div>
+
+        <p>
+        Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake. The
+        longer the sequence you repeat, the greater your score.
+        </p>
+
+        <p>
+        The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for non-profit
+        educational use only. No part of this code or program should be used outside of that definition.
+        </p>
+
+        <div id="quote">
+        <div>Words are cheap. Show me the code.</div>
+        <div>- Linus Torvalds</div>
+      </div>  
     </main>
   );
 }
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import './play.css';
 
 export function Play() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>play displayed here</div>
+      <div class="players">
+        Player:
+        <span class="player-name">Mystery player</span>
+      </div>
+      <ul class="notification">
+        <li class="player-name">Tim started a new game</li>
+        <li class="player-name">Ada started a new game</li>
+        <li class="player-name">Tim scored 337</li>
+      </ul>
+
+      <br />
+
+      <div>
+        <label for="count">Score</label>
+        <input type="text" id="count" value="--" readonly />
+      </div>
+
+      <br />
+
+      <div>
+        <button>Reset</button>
+      </div>
+
+      <br />
+
+      <div>
+        <table>
+          <tr>
+            <td>
+              <button>
+                <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
+                  <path d="M 95,5 95,95 5,95 Q 5,5 95,5" fill="green" />
+                </svg>
+              </button>
+            </td>
+            <td>
+              <button>
+                <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
+                  <path d="M5,5 5,95 95,95 Q 95,5 5,5" fill="red" />
+                </svg>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button>
+                <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
+                  <path d="M5,5 95,5 95,95 Q 5,95 5,5" fill="blue" />
+                </svg>
+              </button>
+            </td>
+            <td>
+              <button>
+                <svg aria-hidden="true" viewBox="0 0 100 100" height="100" width="100">
+                  <path d="M95,5 5,5 5,95 Q 95,95 95,5" fill="yellow" />
+                </svg>
+              </button>
+            </td>
+          </tr>
+        </table>
+      </div>
     </main>
   );
 }
